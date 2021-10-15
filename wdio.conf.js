6ea7a0406cfdc01fa69e68ee7854fabdd634c0d8
preethajega <preethajegatheesan@gmail.com>
2021-10-15 22:58:49 +0530

    new

--- wdio.conf.js
+++ wdio.conf.js
@@ -148,6 +148,10 @@ exports.config = {
        ['junit', 
           {
               outputDir: './report',
+              outputFileFormat: function(options)
+              {
+                  return 'results-${new Date().getTime()}.xml';
+              },
           },
        ],
     ],
